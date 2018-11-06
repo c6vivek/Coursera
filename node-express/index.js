206836ba0d389ff08243204477b66cbeb655ac58
Vivek Gupta <c6vivek@gmail.com>
2018-11-05 19:21:22 -0600

    Express - serve static files

--- node-express/index.js
+++ node-express/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+app.use(morgan('dev')); // http request logger - gives type of request and some other info
+app.use(express.static(__dirname + '/public')); // express component - serves static files
 app.use((request, response, next) => {
     console.log(request.headers);
     response.statusCode = 200;
     response.setHeader('Content-type', 'text/html');
     response.end('<html><body><h1>This is an Express Server</h1></body></html>');
-});
+}); // standard express handler
 
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
