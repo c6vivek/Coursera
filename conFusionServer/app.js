72b8199448e71300d3c6978f25d50cb2f6416a07
Vivek Gupta <c6vivek@gmail.com>
2018-11-12 20:56:42 -0600

    Cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -30,29 +30,41 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(request, response, next) {
-  console.log(request.headers);
-  var authHeader = request.headers.authorization;
-  if (!authHeader) {
-    var error = new Error('You are not Authenticated!');
-    response.setHeader('WWW-Authenticate', 'Basic');
-    error.status = 401;
-    next(error);
-    return;
-  }
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-    next(); // authorized
+  console.log(request.signedCookies);
+
+  if (!request.signedCookies.user) {
+    var authHeader = request.headers.authorization;
+    if (!authHeader) {
+      var error = new Error('You are not Authenticated!');
+      response.setHeader('WWW-Authenticate', 'Basic');
+      error.status = 401;
+      next(error);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+      response.cookie('user','admin',{signed: true});
+      next(); // authorized
+    } else {
+      var error = new Error('You are not authenticated!');
+      response.setHeader('WWW-Authenticate', 'Basic');
+      error.status = 401;
+      next(error);
+    }
   } else {
-    var err = new Error('You are not authenticated!');
-    response.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-  }
+    if (request.signedCookies.user === 'admin') {
+      next();
+    } else {
+      var error = new Error('You are not authenticated!');
+      error.status = 401;
+      next(error);
+    }
+  } 
 }
 app.use(auth);
 
