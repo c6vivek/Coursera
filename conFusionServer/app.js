40e8210112b5ddd4426b032741fc4c3a01ae907a
Vivek Gupta <c6vivek@gmail.com>
2018-11-15 19:51:45 -0600

    Passport authenticaton

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const Dishes = require('./models/dishes');
+var authenticate = require('./authenticate');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -40,6 +42,8 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // function auth(request, response, next) {
 //   //console.log(request.signedCookies);
@@ -79,24 +83,39 @@ app.use(session({
 //     }
 //   }
 // }
+
+// commented to use passport
+// function auth(req, res, next) {
+//   console.log(req.session);
+//   if (!req.session.user) {
+//     var err = new Error('You are not authenticated!');
+//     err.status = 403;
+//     return next(err);
+//   }
+//   else {
+//     if (req.session.user === 'authenticated') {
+//       next();
+//     }
+//     else {
+//       var err = new Error('You are not authenticated!');
+//       err.status = 403;
+//       return next(err);
+//     }
+//   }
+// }
+
 function auth(req, res, next) {
-  console.log(req.session);
-  if (!req.session.user) {
+  console.log(req.user);
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(auth);
